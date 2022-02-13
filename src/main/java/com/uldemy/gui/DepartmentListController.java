f6f936c39fa365a0e69d9624588669aa1ae5557d
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-13 20:14:35 -0300

    Department List Controller

---
+++ src/main/java/com/uldemy/gui/DepartmentListController.java
@@ -0,0 +1,48 @@
+package com.uldemy.gui;
+
+import com.uldemy.application.Main;
+import com.uldemy.model.entities.Department;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentListController implements Initializable {
+
+    @FXML
+    private TableView<Department> departmentTableView;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    @FXML
+    public void onBtnewAction(){
+        System.out.println("onBtnewAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        departmentTableView.prefHeightProperty().bind(stage.heightProperty());
+    }
+}
