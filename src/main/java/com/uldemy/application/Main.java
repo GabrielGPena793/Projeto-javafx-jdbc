99e8dba5177225f715295805818c54b3dda482cd
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-13 17:58:10 -0300

    Main View Design

--- src/main/java/com/uldemy/application/Main.java
+++ src/main/java/com/uldemy/application/Main.java
@@ -3,15 +3,22 @@ package com.uldemy.application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        //ajusta o scrollPane a janela
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Sample JavaFX application");
         stage.setScene(scene);
         stage.show();
